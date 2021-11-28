dd3bf523061395fe823605d65a4b7595fbc5509c
valentyn.burets <Valentyn.Burets@gmail.com>
2021-11-28 22:12:44 +0200

    DSS Algorihm added

---
+++ DSS_App/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace DSS_App
+{
+    partial class DSS
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.logTextBox = new System.Windows.Forms.RichTextBox();
+            this.CreateSignatureButton = new System.Windows.Forms.Button();
+            this.ChooseVerificationFileButton = new System.Windows.Forms.Button();
+            this.ChooseSignatureButton = new System.Windows.Forms.Button();
+            this.VerifyButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textInput
+            // 
+            this.textInput.Location = new System.Drawing.Point(12, 12);
+            this.textInput.Name = "textInput";
+            this.textInput.Size = new System.Drawing.Size(387, 23);
+            this.textInput.TabIndex = 0;
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.Location = new System.Drawing.Point(12, 152);
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.Size = new System.Drawing.Size(545, 249);
+            this.logTextBox.TabIndex = 1;
+            this.logTextBox.Text = "";
+            // 
+            // CreateSignatureButton
+            // 
+            this.CreateSignatureButton.Location = new System.Drawing.Point(418, 11);
+            this.CreateSignatureButton.Name = "CreateSignatureButton";
+            this.CreateSignatureButton.Size = new System.Drawing.Size(139, 23);
+            this.CreateSignatureButton.TabIndex = 2;
+            this.CreateSignatureButton.Text = "Create Signature";
+            this.CreateSignatureButton.UseVisualStyleBackColor = true;
+            this.CreateSignatureButton.Click += new System.EventHandler(this.CreateSignatureButton_Click);
+            // 
+            // ChooseVerificationFileButton
+            // 
+            this.ChooseVerificationFileButton.Location = new System.Drawing.Point(13, 53);
+            this.ChooseVerificationFileButton.Name = "ChooseVerificationFileButton";
+            this.ChooseVerificationFileButton.Size = new System.Drawing.Size(144, 23);
+            this.ChooseVerificationFileButton.TabIndex = 3;
+            this.ChooseVerificationFileButton.Text = "Choose File To Verify Signature";
+            this.ChooseVerificationFileButton.UseVisualStyleBackColor = true;
+            this.ChooseVerificationFileButton.Click += new System.EventHandler(this.ChooseVerificationFileButton_Click);
+            // 
+            // ChooseSignatureButton
+            // 
+            this.ChooseSignatureButton.Location = new System.Drawing.Point(13, 82);
+            this.ChooseSignatureButton.Name = "ChooseSignatureButton";
+            this.ChooseSignatureButton.Size = new System.Drawing.Size(144, 23);
+            this.ChooseSignatureButton.TabIndex = 4;
+            this.ChooseSignatureButton.Text = "Choose Signature";
+            this.ChooseSignatureButton.UseVisualStyleBackColor = true;
+            this.ChooseSignatureButton.Click += new System.EventHandler(this.ChooseSignatureButton_Click);
+            // 
+            // VerifyButton
+            // 
+            this.VerifyButton.Location = new System.Drawing.Point(13, 111);
+            this.VerifyButton.Name = "VerifyButton";
+            this.VerifyButton.Size = new System.Drawing.Size(144, 23);
+            this.VerifyButton.TabIndex = 5;
+            this.VerifyButton.Text = "Verify";
+            this.VerifyButton.UseVisualStyleBackColor = true;
+            this.VerifyButton.Click += new System.EventHandler(this.VerifyButton_Click);
+            // 
+            // DSS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 413);
+            this.Controls.Add(this.VerifyButton);
+            this.Controls.Add(this.ChooseSignatureButton);
+            this.Controls.Add(this.ChooseVerificationFileButton);
+            this.Controls.Add(this.CreateSignatureButton);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.textInput);
+            this.Name = "DSS";
+            this.Text = "DSS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.RichTextBox logTextBox;
+        private System.Windows.Forms.Button CreateSignatureButton;
+        private System.Windows.Forms.Button ChooseVerificationFileButton;
+        private System.Windows.Forms.Button ChooseSignatureButton;
+        private System.Windows.Forms.Button VerifyButton;
+    }
+}
+
---
+++ DSS_App/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DSS_App
+{
+    public partial class DSS : Form
+    {
+        private readonly DSACryptoServiceProvider _dsa = new DSACryptoServiceProvider();
+        private readonly SHA1 _sha1 = SHA1.Create();
+        private string signatureOutput = "";
+        private string chosenFileToVirify = "";
+        private string chosenSignatureFile = "";
+
+        public DSS()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateSignatureButton_Click(object sender, EventArgs e)
+        {
+            if(textInput.Text != "")
+                ProcessSignature(Encoding.Default.GetBytes(textInput.Text));
+            else
+            {
+                try
+                {
+                    using (var openFileDialog = new OpenFileDialog())
+                    {
+                        if (openFileDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            var filePath = openFileDialog.FileName;
+                            LogMessage("File input path is " + filePath);
+                          
+                            ProcessSignature(File.ReadAllBytes(filePath));
+                        }
+                        if (openFileDialog.FileName == "")
+                        {
+                            throw new Exception("The file isn`t chosen");
+                        }
+                    }
+                }catch(Exception ex)
+                {
+                    LogMessage(ex.Message, "ERROR");
+                    LogMessage("Process Creating Signature failed", "ERROR");
+                }
+                
+            }
+        }
+
+        private void ProcessSignature(Byte[] message)
+        {
+            byte[] hash = _sha1.ComputeHash(message);
+            string result = Convert.ToBase64String(_dsa.CreateSignature(hash));
+
+            signatureOutput = result.Trim();
+
+            LogMessage(signatureOutput, "INFO");
+
+            SaveSignature();
+        }
+
+        private void SaveSignature()
+        {
+            if (string.IsNullOrEmpty(signatureOutput))
+            {
+                LogMessage("Can not create signature ", "WARNING");
+                return;
+            }
+
+            var saveFileDialog = new SaveFileDialog
+            {
+                Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*",
+                RestoreDirectory = true,
+                FileName = "Signature.txt"
+            };
+
+            try
+            {
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(saveFileDialog.FileName, signatureOutput);
+                    LogMessage("Signature saved");
+                }
+                else
+                {
+                    throw new Exception("can`t save signature");
+                }
+            }
+            catch(Exception ex)
+            {
+                LogMessage(ex.Message, "ERROR");
+            }
+
+        }
+
+        private void LogMessage(string message, string logLevel = "INFO")
+        {
+            logTextBox.AppendText(
+                $"{DateTime.Now.ToLongTimeString()} [{logLevel}]: {message}\n");
+        }
+
+        private void ChooseVerificationFileButton_Click(object sender, EventArgs e)
+        {
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                try
+                {
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        chosenFileToVirify = openFileDialog.FileName;
+                        LogMessage("Chosen file name to verify is " + chosenFileToVirify);
+                    }
+                    if (chosenFileToVirify == "")
+                    {
+                        throw new Exception("verification file not selected");
+                    }
+                }
+                catch(Exception ex)
+                {
+                    LogMessage(ex.Message, "WARNING");
+                }
+            }
+        }
+
+        private void ChooseSignatureButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var openFileDialog = new OpenFileDialog())
+                {
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        chosenSignatureFile = openFileDialog.FileName;
+                        LogMessage("Chosen signature to verify is " + chosenSignatureFile);
+                    }
+                }
+                if (chosenSignatureFile == "")
+                {
+                    throw new Exception("verification file not selected");
+                }
+            }
+            catch (Exception ex)
+            {
+                LogMessage(ex.Message, "WARNING");
+            }
+
+        }
+
+        private void VerifyButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(chosenFileToVirify) || string.IsNullOrWhiteSpace(chosenFileToVirify))
+            {
+                return;
+            }
+
+            byte[] message = File.ReadAllBytes(chosenFileToVirify);
+            string sign = File.ReadAllText(chosenSignatureFile);
+
+            var result = VerifySignature(message, sign)
+                ? "Verified"
+                : "Not verified";
+
+            
+            LogMessage(result);
+        }
+
+        private bool VerifySignature(byte[] message, string sign)
+        {
+            try
+            {
+                byte[] hash = _sha1.ComputeHash(message);
+                bool verified = _dsa.VerifySignature(hash, Convert.FromBase64String(sign));
+                return verified;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
